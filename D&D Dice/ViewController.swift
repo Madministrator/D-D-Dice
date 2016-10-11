fc5a3b40442a0f99f3f0c028e0c4e37699c83373
VANDER HOEVEN, EVAN <766182@ehs-s-232-14.local>
2016-10-11 10:34:57 -0500

    Initial Commit

---
+++ D&D Dice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  D&D Dice
+//
+//  Created by VANDER HOEVEN, EVAN on 10/11/16.
+//  Copyright © 2016 VANDER HOEVEN, EVAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ D&D DiceTests/D_D_DiceTests.swift
@@ -0,0 +1,36 @@
+//
+//  D_D_DiceTests.swift
+//  D&D DiceTests
+//
+//  Created by VANDER HOEVEN, EVAN on 10/11/16.
+//  Copyright © 2016 VANDER HOEVEN, EVAN. All rights reserved.
+//
+
+import XCTest
+@testable import D_D_Dice
+
+class D_D_DiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
